b7a94efc8e7cb9a0c4a67b141fec68da8df1bf6f
Marin Bošnjak <Marin Bonjak>
2016-12-04 22:54:47 +0100

    event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,19 @@ export class Hero {
 
       <h2>My Heroes</h2>
       <ul class="heroes">
-        <li *ngFor="let hero of heroes">
+        <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
           <span class="badge">{{hero.id}}</span> {{hero.name}}
+        </li>
       </ul>
 
+      <h2>{{selectedHero.name}} details!</h2>
+      <div>
+        <label>id: </label>
+        {{selectedHero.id}}
+      </div>
+
       <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
+      <input [(ngModel)]="selectedHero.name" placeholder="name">
     </div>
     `
 })
@@ -28,6 +35,12 @@ export class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   heroes: Hero[] = HEROES;
 }
