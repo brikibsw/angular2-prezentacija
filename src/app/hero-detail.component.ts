2e465e1391aa0797a7330faf9aa552b44a87fe86
Marin Bošnjak <Marin Bonjak>
2016-12-05 13:19:11 +0100

    Styles

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -11,6 +11,7 @@ import "rxjs/add/operator/switchMap";
   moduleId: module.id.toString(),
   templateUrl: "hero-detail.component.html",
   selector: 'my-hero-detail',
+  styleUrls: [ "hero-detail.component.css" ]
 })
 export class HeroDetailComponent implements OnInit {
   constructor(
