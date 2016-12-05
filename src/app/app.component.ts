852343c3d85259709590cbabb281717acf4470e5
brikibsw <briki.bsw@gmail.com>
2016-12-05 22:00:49 +0100

    fix: closing li element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class Hero {
       <ul class="heroes">
         <li *ngFor="let hero of heroes">
           <span class="badge">{{hero.id}}</span> {{hero.name}}
+        </li>
       </ul>
 
       <label>name: </label>
