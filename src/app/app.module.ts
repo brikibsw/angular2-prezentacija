cedc1b67cb668914e33dd06751ce82555479f7ef
Marin Bošnjak <Marin Bonjak>
2016-12-05 13:00:24 +0100

    Refactored routes, put it in separate file/module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from "@angular/router";
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from "./heroes.component"; 
